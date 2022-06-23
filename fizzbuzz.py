d1225f96b4504b2d9f2f47485fe74a2a98e2971c
imTempie <harleygoogoo@gmail.com>
2022-06-23 20:46:28 +0100

    Created and Completed Fizz Buzz

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+#Divide by 3 = fizz
+#Divide by 5 = buzz
+#Divide by 3 and 5 = fizzbuzz
+
+while True:
+
+    x = int(input("Enter Number:    "))
+    
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
