d6c5ee01eb2a685da30f83cdc5f99e0b725ea6d2
imTempie <harleygoogoo@gmail.com>
2022-06-23 20:47:56 +0100

    Added comment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
 #Divide by 3 = fizz
 #Divide by 5 = buzz
 #Divide by 3 and 5 = fizzbuzz
+#None of the above = print x
 
 while True:
 
     x = int(input("Enter Number:    "))
-    
+
     if x % 3 == 0 and x % 5 == 0:
         print("FizzBuzz")
     elif x % 3 == 0:
